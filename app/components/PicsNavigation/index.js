c2092f682ba52f7649c49d88a54b5472c73305a0
Hope <hopekim511@gmail.com>
2017-07-12 23:04:11 -0700

    Setting the cameraImages state directly from the import function within selectAppropriateImages with a concatonated array so that the images show up after the promise is resolved

--- app/components/PicsNavigation/index.js
+++ app/components/PicsNavigation/index.js
@@ -6,10 +6,10 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { Flex, Box } from 'grid-styled';
+import {Flex, Box} from 'grid-styled';
 
 const pathToCuriosityCameraImages = require.context('assets/cameras/Curiosity');
-const curiosityCameras = ['FHAZ.jpg', 'NAVCAM.jpg', 'MAST.jpg'];
+const curiosityCameras            = ['FHAZ.jpg', 'NAVCAM.jpg', 'MAST.jpg'];
 
 const Wrapper = styled.div`
     position: absolute;
@@ -21,89 +21,63 @@ const H4 = styled.h4`
 
 `;
 
-const Ul = styled.ul`
-  padding: 0;
-`;
-
 const CameraImage = styled.div`
       background-position: 50%;
     background-size: cover;
     height: 250px;
+        border-radius: 5px;
 `;
 
 class PicsNavigation extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            readyToRender: false,
+            readyToRenderImages: false,
             noOfCameras: "",
             latestEarthDate: "",
             curiosityCameras: ['FHAZ.jpg', 'NAVCAM.jpg', 'MAST.jpg'],
             cameraImages: [],
         }
 
-        this.dynamicImport = this.dynamicImport.bind(this);
+        this.dynamicImport           = this.dynamicImport.bind(this);
         this.selectAppropriateImages = this.selectAppropriateImages.bind(this);
     }
 
-    componentDidMount(){
+    dynamicImport(path) {
+        return import(`assets/cameras/Curiosity/${path}`);
+    }
+
+    selectAppropriateImages(rover) {
+        this.state.curiosityCameras.map(imgPath =>
+            this.dynamicImport(imgPath).then(path => {
+                const imageArray = this.state.cameraImages.concat(path);
+                this.setState({cameraImages: imageArray});
+            }).catch(error => console.log(error))
+        );
+    }
+
+    componentWillMount() {
         console.log(this.props);
         this.setState({
             rover: this.props.selectedRover,
             noOfCameras: this.props.cameras.length,
             latestEarthDate: this.props.latestEarthDate,
         });
-
         this.selectAppropriateImages(this.state.rover);
     }
 
-    dynamicImport(path){
-        return import(`assets/cameras/Curiosity/${path}`);
-    }
-
-    selectAppropriateImages(rover){
-        const imageFiles = [];
-
-        this.state.curiosityCameras.map(imgPath=>
-            this.dynamicImport(imgPath)
-            .then(path=>imageFiles.push(path))
-            .catch(error=>console.log(error))
-        );
-
-        this.setState({
-            cameraImages: imageFiles,
-            readyToRender: true,
-        })
-
-
-        // switch(rover){
-        //     case "Curiosity":
-        //         curiosityCameras.map(imgFile=>
-        //             this.setState(prevState=>{
-        //                 cameraImages: prevState.push(pathToCuriosityCameraImages(imgFile, true))
-        //             })
-        //         )
-        //         break;
-        //     default:
-        //         return;
-        // }
-    }
-
     render() {
-        const widthOfWrapper =  {width: 25 * this.state.noOfCameras + "%"};
+        const widthOfWrapper = {width: 25 * this.state.noOfCameras + "%"};
 
-        // console.log(this.state);
-        // style={{backgroundImage: "url(" + this.state.cameraImages[i] + ")"}}
         return (
             <Wrapper style={widthOfWrapper}>
                 <H4>{this.state.latestEarthDate}</H4>
                 <Flex>
                     {this.props.cameras.map((camera, i) =>
-                        <Box flex='1' m="16px">
+                        <Box flex='1' m="16px" key={i}>
                             <CameraImage
-                                style={{backgroundImage:"url(" + this.state.cameraImages[i] + ")"}}
+                                style={{backgroundImage: "url(" + this.state.cameraImages[i] + ")"}}
                                 data-camera={camera.name}
-                                key={i}
                                 onClick={() => this.props.fetchPictures(i)}>
                                 {camera.full_name}
                             </CameraImage>
