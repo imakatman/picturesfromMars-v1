afac84bb9208521c8ea2e7cd75e6c5d0cf872b80
Hope <hopekim511@gmail.com>
2017-08-13 13:18:24 -0700

    Deleted SelectARover and moved all the code inside of Picker

--- app/components/Picker/index.js
+++ app/components/Picker/index.js
@@ -6,12 +6,34 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import SelectARover from 'components/SelectARover';
+import styled from 'styled-components';
+import { Link } from 'react-router';
 import { Flex, Box } from 'grid-styled';
 
 import Curiosity from 'assets/rovers/Curiosity.jpg';
 import Opportunity from 'assets/rovers/Opportunity.jpg';
 import Spirit from 'assets/rovers/Spirit.jpg';
+
+import { TopLayer, Overlay } from '../StyledComponents/Overlay';
+import { Heading, SubHeading } from '../StyledComponents/Headings';
+import { SmallLabel } from '../StyledComponents/SmallLabel';
+
+
+const HeadingLink = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+const RoverColumn = styled.div`
+    position: relative;
+    background-size: cover;
+    background-position: ${props => props.name === 'Curiosity' ? '40%' : '50%'};
+    height: 100vh;
+    display: table-cell;
+    vertical-align: middle;
+    width: 100vw;
+    text-align: center;
+`;
 
 class Picker extends React.Component {
   constructor(props) {
@@ -27,12 +49,28 @@ class Picker extends React.Component {
       <Flex>
         {this.props.values.map((value, i) =>
           <Box flex={1} key={value.id}>
-            <SelectARover
-              portrait={this.state.portrait[i][value.name]}
-              name={value.name}
-              totalPhotos={value.total_photos}
-              activeState={this.props.activeState[i]}
-            />
+            <div>
+              <HeadingLink to={'/r/' + value.name}>
+                <RoverColumn
+                  active={this.props.activeState[i]}
+                  name={value.name}
+                  style={{ backgroundImage: 'url(' + this.state.portrait[i][value.name] + ')' }}>
+                  <TopLayer>
+                    <Heading color={'white'}>
+                      {value.name}
+                    </Heading>
+                    <SubHeading color={'white'}>
+                      <SmallLabel>Total Photos</SmallLabel>
+                      {value.total_photos}
+                    </SubHeading>
+                    <SubHeading color={'white'}>
+                      <SmallLabel>Total Photos</SmallLabel>
+                    </SubHeading>
+                  </TopLayer>
+                  <Overlay opacity='0.3' />
+                </RoverColumn>
+              </HeadingLink>
+            </div>
           </Box>
         )}
       </Flex>
